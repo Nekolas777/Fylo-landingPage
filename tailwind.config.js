df5ff8070117a84aef83be0a3a951e2da5793cda
Nekolas777 <u202217485@upc.edu.pe>
2024-02-25 19:22:45 -0500

    iddentation fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,14 +7,14 @@ export default {
   theme: {
     extend: {
       colors: {
-				'primary': 'hsl(0, 0%, 100%)',
-				'secondary': 'hsl(217, 28%, 15%)',
-				'footer': 'hsl(216, 53%, 9%)',
+        'primary': 'hsl(0, 0%, 100%)',
+        'secondary': 'hsl(217, 28%, 15%)',
+        'footer': 'hsl(216, 53%, 9%)',
         'testimonials': 'hsl(219, 30%, 18%)',
         'cta': 'hsl(198, 60%, 50%)',
         'cta_hover': 'hsl(176, 68%, 64%)',
         'error': 'hsl(0, 100%, 63%)'
-			},
+      },
       fontFamily: {
         'raleway': ["Raleway", "sans-serif"],
         'sans': ["Opens Sans", "sans-serif"]
